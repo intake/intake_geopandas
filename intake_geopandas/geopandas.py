fe5d400430ef71f8183ec2d9f1685abe0a76c2f3
Ian Rose <ian.r.rose@gmail.com>
2019-03-28 09:55:37 -0700

    Add postgis source.

--- intake_geopandas/geopandas.py
+++ intake_geopandas/geopandas.py
@@ -12,30 +12,9 @@ class GeoPandasSource(DataSource, ABC):
     container = 'dataframe'
     partition_access = True
 
-    def __init__(self, urlpath, bbox=None, geopandas_kwargs=None, metadata=None):
-        """
-        Parameters
-        ----------
-        urlpath : str or iterable, location of data
-            Either the absolute or relative path to the file or URL to be opened.
-            Some examples:
-            - ``{{ CATALOG_DIR }}data/states.shp``
-            - ``http://some.domain.com/data/dtates.shp``
-        bbox : tuple | GeoDataFrame or GeoSeries, default None
-            Filter features by given bounding box, GeoSeries, or GeoDataFrame.
-            CRS mis-matches are resolved if given a GeoSeries or GeoDataFrame.
-        geopandas_kwargs : dict
-            Any further arguments to pass to geopandas's read_* function.
-        """
-        self.urlpath = urlpath
-        self._bbox = bbox
-        self._geopandas_kwargs = geopandas_kwargs or {}
-        self._dataframe = None
-
-        super(GeoPandasSource, self).__init__(metadata=metadata)
 
     @abstractmethod
-    def _open_dataset(self, urlpath):
+    def _open_dataset(self):
         """
         Open dataset using geopandas and use pattern fields to set new columns.
         """
@@ -43,7 +22,7 @@ class GeoPandasSource(DataSource, ABC):
 
     def _get_schema(self):
         if self._dataframe is None:
-            self._open_dataset(self.urlpath)
+            self._open_dataset()
 
         dtypes = self._dataframe.dtypes.to_dict()
         dtypes = {n: str(t) for (n, t) in dtypes.items()}
@@ -68,14 +47,65 @@ class GeoPandasSource(DataSource, ABC):
         self._dataframe = None
 
 class GeoPandasFileSource(GeoPandasSource):
-    def _open_dataset(self, urlpath):
+    def __init__(self, uri, bbox=None, geopandas_kwargs=None, metadata=None):
+        """
+        Parameters
+        ----------
+        uri : str or iterable, location of data
+            Either the absolute or relative path to the file or URL to be opened.
+            Some examples:
+            - ``{{ CATALOG_DIR }}data/states.shp``
+            - ``http://some.domain.com/data/states.geo.json``
+        bbox : tuple | GeoDataFrame or GeoSeries, default None
+            Filter features by given bounding box, GeoSeries, or GeoDataFrame.
+            CRS mis-matches are resolved if given a GeoSeries or GeoDataFrame.
+        geopandas_kwargs : dict
+            Any further arguments to pass to geopandas's read_file function.
+        """
+        self.uri = uri
+        self._bbox = bbox
+        self._geopandas_kwargs = geopandas_kwargs or {}
+        self._dataframe = None
+
+        super().__init__(metadata=metadata)
+
+    def _open_dataset(self):
         """Open dataset using geopandas and use pattern fields to set new columns
         """
         self._dataframe = geopandas.read_file(
-            urlpath, bbox=self._bbox, **self._geopandas_kwargs)
+            self.uri, bbox=self._bbox, **self._geopandas_kwargs)
 
 class GeoJSONSource(GeoPandasFileSource):
-    name='geojson'
+    name="geojson"
 
 class ShapefileSource(GeoPandasFileSource):
-    name='shapefile'
+    name="shapefile"
+
+class PostGISSource(GeoPandasSource):
+    name="postgis"
+
+    def __init__(self, uri, sql_expr=None, table=None, geopandas_kwargs=None, metadata=None):
+        """
+        Parameters
+        ----------
+        uri : str
+            The connection string for the PostGIS database.
+        geopandas_kwargs : dict
+            Any further arguments to pass to geopandas's read_postgis function.
+        """
+        self.uri = uri
+        if sql_expr:
+            self.sql_expr = sql_expr
+        elif table:
+            self.sql_expr = f"SELECT * FROM {table}"
+        else:
+            raise ValueError("Must provide either a sql_expr or a table")
+
+        self._geopandas_kwargs = geopandas_kwargs or {}
+        self._dataframe = None
+
+        super().__init__(metadata=metadata)
+
+    def _open_dataset(self):
+        self._dataframe = geopandas.read_postgis(
+            self.sql_expr, self.uri, **self._geopandas_kwargs)
