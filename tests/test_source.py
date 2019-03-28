aea42ee3fadf8363a7d651c3873a90dcd98510c1
Ian Rose <ian.r.rose@gmail.com>
2019-03-28 10:48:26 -0700

    Update tests.

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,29 +1,39 @@
 # -*- coding: utf-8 -*-
-import numpy as np
-import pandas as pd
 import os
 import pytest
-from unittest.mock import patch
 
-from intake_geopandas import geopandas
-
+from intake_geopandas import GeoJSONSource, ShapefileSource
 
 @pytest.fixture
-def data_filenames():
+def shape_filenames():
     basedir = os.path.dirname(__file__)
     return dict(stations=os.path.join(basedir, 'data', 'stations', 'stations.shp'))
 
 
 @pytest.fixture
-def stations_datasource(data_filenames):
-    return geopandas.ShapeSource(data_filenames['stations'])
+def shape_datasource(shape_filenames):
+    return ShapefileSource(shape_filenames['stations'])
+
+@pytest.fixture
+def geojson_filenames():
+    basedir = os.path.dirname(__file__)
+    return dict(countries=os.path.join(basedir, 'data', 'countries.geo.json'))
 
 
-def test_stations_datasource(stations_datasource):
-    info = stations_datasource.discover()
+@pytest.fixture
+def geojson_datasource(geojson_filenames):
+    return GeoJSONSource(geojson_filenames['countries'])
+
+
+def test_shape_datasource(shape_datasource):
+    info = shape_datasource.discover()
 
     assert info['dtype'] == {'name': 'object',
                              'marker-col': 'object',
                              'marker-sym': 'object',
                              'line': 'object',
                              'geometry': 'object'}
+
+def test_countries_datasource(geojson_datasource):
+    info = geojson_datasource.discover()
+    print(info)
