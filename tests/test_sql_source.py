868b611d28379e38e74dcc91544f199baa532a0d
Ian Rose <ian.r.rose@gmail.com>
2019-04-10 12:13:06 -0700

    Begin working on SpatiaLite tests by using modified copies of those in GeoPandas.

---
+++ tests/test_sql_source.py
@@ -0,0 +1,53 @@
+"""
+Tests for loading data from SQL data sources.
+Modified from those in the GeoPandas test suite.
+
+In order to run, SpatiaLite must be installed and configured.
+"""
+import pytest
+
+import geopandas
+from geopandas import read_postgis, read_file
+from .util import connect_spatialite, create_spatialite, validate_boro_df
+
+@pytest.fixture
+def df_nybb():
+    nybb_path = geopandas.datasets.get_path('nybb')
+    df = read_file(nybb_path)
+    return df
+
+
+class TestIO:
+                
+    def test_read_spatialite_null_geom(self, df_nybb):
+        """Tests that geometry with NULL is accepted."""
+        try:
+            con = connect_spatialite()
+        except Exception:
+            raise pytest.skip()
+        else:
+            geom_col = df_nybb.geometry.name
+            df_nybb.geometry.iat[0] = None
+            create_spatialite(con, df_nybb)
+            sql = 'SELECT ogc_fid, borocode, boroname, shape_leng, shape_area, AsEWKB("{0}") AS "{0}" FROM nybb'.format(geom_col)
+            df = read_postgis(sql, con, geom_col=geom_col)
+            validate_boro_df(df)
+        finally:
+            if 'con' in locals():
+                con.close()
+
+    def test_read_spatialite_binary(self, df_nybb):
+        """Tests that geometry read as binary is accepted."""
+        try:
+            con = connect_spatialite()
+        except Exception:
+            raise pytest.skip()
+        else:
+            geom_col = df_nybb.geometry.name
+            create_spatialite(con, df_nybb)
+            sql = 'SELECT ogc_fid, borocode, boroname, shape_leng, shape_area, ST_AsBinary("{0}") AS "{0}" FROM nybb'.format(geom_col)
+            df = read_postgis(sql, con, geom_col=geom_col)
+            validate_boro_df(df)
+        finally:
+            if 'con' in locals():
+                con.close()
---
+++ tests/util.py
@@ -0,0 +1,91 @@
+"""Modified from geopandas/tests/util.py"""
+
+import sqlite3
+
+from geopandas import GeoDataFrame
+from geopandas.testing import (
+    geom_equals, geom_almost_equals, assert_geoseries_equal)  # flake8: noqa
+from pandas import Series
+
+
+def validate_boro_df(df, case_sensitive=False):
+    """Tests a GeoDataFrame that has been read in from the nybb dataset."""
+    assert isinstance(df, GeoDataFrame)
+    # Make sure all the columns are there and the geometries
+    # were properly loaded as MultiPolygons
+    assert len(df) == 5
+    columns = ('BoroCode', 'BoroName', 'Shape_Leng', 'Shape_Area')
+    if case_sensitive:
+        for col in columns:
+            assert col in df.columns
+    else:
+        for col in columns:
+            assert col.lower() in (dfcol.lower() for dfcol in df.columns)
+    assert Series(df.geometry.type).dropna().eq('MultiPolygon').all()
+
+
+def get_srid(df):
+    """Return srid from `df.crs`."""
+    crs = df.crs
+    return (int(crs['init'][5:]) if 'init' in crs
+                                 and crs['init'].startswith('epsg:')
+            else 0)
+
+
+def connect_spatialite():
+    """
+    Return a memory-based SQLite3 connection with SpatiaLite enabled & initialized.
+
+    `The sqlite3 module must be built with loadable extension support <https://docs.python.org/3/library/sqlite3.html#f1>`_ and `SpatiaLite <https://www.gaia-gis.it/fossil/libspatialite/index>`_ must be available on the system as a SQLite module.
+    Packages available on Anaconda meet requirements.
+
+    Exceptions
+    ----------
+    ``AttributeError`` on missing support for loadable SQLite extensions
+    ``sqlite3.OperationalError`` on missing SpatiaLite
+    """
+    try:
+        with sqlite3.connect(':memory:') as con:
+            con.enable_load_extension(True)
+            con.load_extension('mod_spatialite')
+            con.execute('SELECT InitSpatialMetaData(TRUE)')
+    except Exception:
+        con.close()
+        raise
+    return con
+
+def create_spatialite(con, df):
+    """
+    Return a SpatiaLite connection containing the nybb table.
+
+    Parameters
+    ----------
+    `con`: ``sqlite3.Connection``
+    `df`: ``GeoDataFrame``
+    """
+
+    with con:
+        geom_col = df.geometry.name
+        srid = get_srid(df)
+        con.execute('CREATE TABLE IF NOT EXISTS nybb '
+                    '( ogc_fid INTEGER PRIMARY KEY'
+                    ', borocode INTEGER'
+                    ', boroname TEXT'
+                    ', shape_leng REAL'
+                    ', shape_area REAL'
+                    ')')
+        con.execute('SELECT AddGeometryColumn(?, ?, ?, ?)',
+                    ('nybb', geom_col, srid, df.geom_type.dropna().iat[0].upper()))
+        con.execute('SELECT CreateSpatialIndex(?, ?)', ('nybb', geom_col))
+        sql_row = "INSERT INTO nybb VALUES(?, ?, ?, ?, ?, GeomFromText(?, ?))"
+        con.executemany(sql_row,
+                        ((None,
+                          row.BoroCode,
+                          row.BoroName,
+                          row.Shape_Leng,
+                          row.Shape_Area,
+                          row.geometry.wkt if row.geometry
+                          else None,
+                          srid
+                         ) for row in df.itertuples(index=False)))
+    return con
