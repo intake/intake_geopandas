717b8f541b64bfc0111193fe7aa47ecebce014a4
AS <aaron.spring@mpimet.mpg.de>
2020-07-22 15:40:30 +0200

    more tests

--- tests/test_file_source.py
+++ tests/test_file_source.py
@@ -1,74 +1,57 @@
 # -*- coding: utf-8 -*-
 import os
-import pytest
 
+import pytest
 from pkg_resources import get_distribution, parse_version
 
 from intake_geopandas import GeoJSONSource, ShapefileSource
 
 geom_col_type = (
-    "object"
-    if parse_version(get_distribution("geopandas").version) < parse_version("0.6")
-    else "geometry"
+    'object'
+    if parse_version(get_distribution('geopandas').version) < parse_version('0.6')
+    else 'geometry'
 )
 
 
 @pytest.fixture
 def shape_filenames():
     basedir = os.path.dirname(__file__)
-    return dict(stations=os.path.join(basedir, "data", "stations", "stations.shp"))
+    return dict(stations=os.path.join(basedir, 'data', 'stations', 'stations.shp'))
 
 
 @pytest.fixture
 def shape_datasource(shape_filenames):
-    return ShapefileSource(shape_filenames["stations"])
+    return ShapefileSource(shape_filenames['stations'])
 
 
 @pytest.fixture
 def geojson_filenames():
     basedir = os.path.dirname(__file__)
-    return dict(countries=os.path.join(basedir, "data", "countries.geo.json"))
+    return dict(countries=os.path.join(basedir, 'data', 'countries.geo.json'))
 
 
 @pytest.fixture
 def geojson_datasource(geojson_filenames):
-    return GeoJSONSource(geojson_filenames["countries"])
+    return GeoJSONSource(geojson_filenames['countries'])
 
 
 def test_shape_datasource(shape_datasource):
     info = shape_datasource.discover()
     shape_datasource.read()
-    assert info["dtype"] == {
-        "name": "object",
-        "marker-col": "object",
-        "marker-sym": "object",
-        "line": "object",
-        "geometry": geom_col_type,
+    assert info['dtype'] == {
+        'name': 'object',
+        'marker-col': 'object',
+        'marker-sym': 'object',
+        'line': 'object',
+        'geometry': geom_col_type,
     }
 
 
 def test_countries_datasource(geojson_datasource):
     info = geojson_datasource.discover()
     geojson_datasource.read()
-    assert info["dtype"] == {
-        "geometry": geom_col_type,
-        "id": "object",
-        "name": "object",
+    assert info['dtype'] == {
+        'geometry': geom_col_type,
+        'id': 'object',
+        'name': 'object',
     }
-
-
-def test_cache():
-    import intake
-    item = intake.open_catalog('tests/data/shape.catalog.yaml')['MEOW_cache']
-    if isinstance(item.cache[0], str):
-        item.cache[0].clear_cache()
-    # read new
-    shp = item.read()
-    urlpath = item.urlpath
-    expected_location_on_disk = item.cache[0]._path(urlpath)
-    assert isinstance(expected_location_on_disk, str)
-    import geopandas
-    assert isinstance(shp, geopandas.GeoDataFrame)
-    import os
-    assert os.path.exists(expected_location_on_disk)
-
---
+++ tests/test_remote_source_and_cache.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+
+import geopandas
+import intake
+import pytest
+from fiona.errors import DriverError
+
+from intake_geopandas import GeoJSONSource, ShapefileSource
+
+
+def try_clean_old_cache(item):
+    if isinstance(item.cache, list):
+        if len(item.cache) > 0:
+            if isinstance(item.cache[0], str):
+                item.cache[0].clear_cache()
+
+
+def try_clean_new_cache(item):
+    c = None  # default
+    for c in ['blockcache', 'filecache', 'simplecache']:
+        if c in item.storage_options:
+            caching = c
+    assert c is not None, 'caching not found'
+    path = item.storage_options[caching]['cache_storage']
+    if isinstance(path, str):
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+
+@pytest.mark.parametrize('cat_item', ['MEOW', 'MEOW_new_cache', 'MEOW_old_cache'])
+def test_cache(cat_item):
+    """Test new fsspec and old intake caching."""
+    item = intake.open_catalog('tests/data/shape.catalog.yaml')[cat_item]
+
+    # delete previous caching
+    if 'cache' in cat_item and 'old' in cat_item:
+        try_clean_old_cache(item)
+    elif 'new' in cat_item:
+        try_clean_new_cache(item)
+    # read new
+    shp = item.read()
+    assert isinstance(shp, geopandas.GeoDataFrame)
+    # test caching
+    if 'cache' in cat_item:
+        if 'old' in cat_item:
+            urlpath = item.urlpath
+            expected_location_on_disk = item.cache[0]._path(urlpath)
+        elif 'new' in cat_item:
+            expected_location_on_disk = item.storage_options['simplecache'][
+                'cache_storage'
+            ]
+        assert isinstance(expected_location_on_disk, str)
+        assert os.path.exists(expected_location_on_disk)
+        # delete caching
+        if 'old' in cat_item:
+            try_clean_old_cache(item)
+        elif 'new' in cat_item:
+            try_clean_new_cache(item)
+
+
+@pytest.mark.parametrize('same_names', [False, True])
+def test_same_name_required_else_warn(same_names):
+    """Test that same_names is required to load zip file from cache. Warns during init
+    if same_names is False for zip file."""
+    ShapefileSource_args = {
+        'urlpath': 'simplecache::http://maps.tnc.org/files/shp/MEOW-TNC.zip',
+        'storage_options': {
+            'simplecache': {'same_names': same_names, 'cache_storage': 'tmpfile'}
+        },
+    }
+    if not same_names:
+        with pytest.warns(UserWarning, match='same_names = True'):
+            item = ShapefileSource(**ShapefileSource_args)
+    else:
+        item = ShapefileSource(**ShapefileSource_args)
+    expected_location_on_disk = item.storage_options['simplecache']['cache_storage']
+    try_clean_new_cache(item)
+    assert not os.path.exists(expected_location_on_disk)
+    if not same_names:
+        with pytest.raises(DriverError):
+            item.read()
+    else:
+        item.read()
+    assert os.path.exists(expected_location_on_disk)
+    try_clean_new_cache(item)
+    assert not os.path.exists(expected_location_on_disk)
+
+
+@pytest.fixture
+def GeoJSONSource_countries_remote():
+    url = 'simplecache::https://raw.githubusercontent.com/intake/intake_geopandas/master/tests/data/countries.geo.json'
+    return GeoJSONSource(
+        **{
+            'urlpath': url,
+            'storage_options': {'simplecache': {'cache_storage': 'tempfile'}},
+        }
+    )
+
+
+@pytest.mark.parametrize('same_names', [False, True])
+def test_remote_GeoJSONSource(GeoJSONSource_countries_remote, same_names):
+    """GeoJSONSource works with either `same_names` True or False."""
+    item = GeoJSONSource_countries_remote
+    item.storage_options['simplecache']['same_names'] = same_names
+    item.read()
+    expected_location_on_disk = item.storage_options['simplecache']['cache_storage']
+    assert os.path.exists(expected_location_on_disk)
+    try_clean_new_cache(item)
+    assert not os.path.exists(expected_location_on_disk)
